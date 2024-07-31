c35fcccdf634141b7046aa93978529fd10a3eb44
Arne Becker <arbecker@stroeer.de>
2024-07-31 17:36:41 +0200

    make select placeholder translateable

--- resources/lang/en/filament-value-range-filter.php
+++ resources/lang/en/filament-value-range-filter.php
@@ -2,6 +2,7 @@
 
 return [
 
+    'range.placeholder' => 'Select condition',
     'range.options.equal' => 'is equal to',
     'range.options.between' => 'is between',
     'range.options.greater_than' => 'is greater than',
