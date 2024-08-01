358e15f665abff171ed7c9fa8b0a71f3c231516f
Arne Becker <arbecker@stroeer.de>
2024-08-01 10:40:12 +0200

    add german translations

---
+++ resources/lang/de/filament-value-range-filter.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    'range.placeholder' => 'Alle',
+    'range.options.equal' => 'ist gleich',
+    'range.options.not_equal' => 'ist ungleich',
+    'range.options.between' => 'ist zwischen',
+    'range.options.greater_than' => 'ist größer als',
+    'range.options.greater_than_equal' => 'ist größer als oder gleich',
+    'range.options.less_than' => 'ist kleiner als',
+    'range.options.less_than_equal' => 'ist kleiner als oder gleich',
+    'range.indicator.equal' => ':label ist gleich :value',
+    'range.indicator.not_equal' => ':label ist ungleich :value',
+    'range.indicator.between' => ':label ist zwischen :fromValue und :toValue',
+    'range.indicator.greater_than' => ':label ist größer als :value',
+    'range.indicator.greater_than_equal' => ':label ist größer als oder gleich :value',
+    'range.indicator.less_than' => ':label ist kleiner als :value',
+    'range.indicator.less_than_equal' => ':label ist kleiner als oder gleich :value',
+
+];
