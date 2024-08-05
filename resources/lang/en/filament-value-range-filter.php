b5c0805c2da7576ec431a3327db30064044a3296
Arne Becker <arbecker@stroeer.de>
2024-08-05 10:26:34 +0200

    fix typo in translation string

--- resources/lang/en/filament-value-range-filter.php
+++ resources/lang/en/filament-value-range-filter.php
@@ -11,7 +11,7 @@ return [
     'range.options.less_than_equal' => 'is less than or equal to',
     'range.indicator.equal' => ':label is equal to :value',
     'range.indicator.not_equal' => ':label is not equal to :value',
-    'range.indicator.between' => ':label is between :formValue and :toValue',
+    'range.indicator.between' => ':label is between :fromValue and :toValue',
     'range.indicator.greater_than' => ':label is greater than :value',
     'range.indicator.greater_than_eqal' => ':label is greater than or equal to :value',
     'range.indicator.less_than' => ':label is less than :value',
