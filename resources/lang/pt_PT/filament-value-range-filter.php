e645365836162db46ad34d570338d7b17d9c76f6
Paulo Pimentel <paulocostapimentel@gmail.com>
2024-11-19 20:43:56 -0100

    added pt_PT translation

---
+++ resources/lang/pt_PT/filament-value-range-filter.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    'range.placeholder' => 'Selecione a condição',
+    'range.options.equal' => 'é igual a',
+    'range.options.not_equal' => 'é diferente de',
+    'range.options.between' => 'está entre',
+    'range.options.greater_than' => 'é maior que',
+    'range.options.greater_than_equal' => 'é maior ou igual a',
+    'range.options.less_than' => 'é menor que',
+    'range.options.less_than_equal' => 'é menor ou igual a',
+    'range.indicator.equal' => ':label é igual a :value',
+    'range.indicator.not_equal' => ':label é diferente de :value',
+    'range.indicator.between' => ':label está entre :fromValue e :toValue',
+    'range.indicator.greater_than' => ':label é maior que :value',
+    'range.indicator.greater_than_equal' => ':label é maior ou igual a :value',
+    'range.indicator.less_than' => ':label é menor que :value',
+    'range.indicator.less_than_equal' => ':label é menor ou igual a :value',
+
+];
